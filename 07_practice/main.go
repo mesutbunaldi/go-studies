60beb49d3cc90a3a59be422b42a8185498f7b9d4
Mesut <mesut.bunaldi@netone.net.tr>
2022-06-06 01:02:35 +0300

    07_practice is added

---
+++ 07_practice/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var studentName string = "John Doe"
+	var grade int = 77
+	var isPassed bool = true
+
+	fmt.Println(studentName)
+	fmt.Println(grade)
+	fmt.Println(isPassed)
+
+}
